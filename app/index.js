a69b13f3aef5021a81bdca1ebeff2727e45e379f
yaser ahmadi <m.y.ahmadi22@gmail.com>
2022-08-17 14:44:55 +0430

    remove request script

--- app/index.js
+++ app/index.js
@@ -1,22 +1,16 @@
-// index.js
 require('dotenv').config();
 const express = require('express');
 const app = express();
-let count = 0;
 app.get('/', (req, res) => {
-    try {
-        let number = 100000
-        for (let i = 0; i < number; i++) {
-            let innerNumber = 106600
-            for (let j = 0; j < innerNumber; j++) {
-                
-            }
-        }
-        res.send(`Hello World! server ${process.env.NAME}`)
-    } catch (error) {
-        throw new Error('BROKEN') 
+    const loop = 100000
+    for (let i = 0; i < loop; i++) {
+        const innerLoob = 100000
+        for (let j = 0; j < innerLoob; j++) { }
     }
-
+    res.send(`Finished task in ${process.env.NODE_ENV}`)
 })
-
-app.listen(5000, () => console.log(`YAssssssssssssssssssssss  ${process.env.NAME}`));
+const port = process.env.PORT;
+app.listen(5000, () => {
+    console.log(`Server is running on Prot: ${port}`);
+    console.log(`Envirement: ${process.env.NODE_ENV}`);
+})
